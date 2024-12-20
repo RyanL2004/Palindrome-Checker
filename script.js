9f16364b25c91c179b1f62ee7e2af395471105bc
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-19 19:52:26 -0500

    feat: add input validation to ensure a value is entered

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const inputText = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
+
+const renderAlert = () => {
+  if (inputText.value === "") {
+    alert("Please input a value");
+    return false;
+  } else {
+    return true;
+  }
+};
